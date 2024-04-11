4916b525ee13c137e58d4750e2bd941d7490882c
Jacob Quintero <jquintero9966@tamu.edu>
2024-04-11 12:36:31 -0500

    Completed Task 3 with minor shadow error

--- A6/src/main.cpp
+++ A6/src/main.cpp
@@ -18,6 +18,7 @@
 // functions. You can also say `using std::cout` to be more selective.
 // You should never do this in a header file.
 using namespace std;
+const double EPSILON = 1e-5;
 
 class Vec3 {
 public:
@@ -53,24 +54,134 @@ public:
 	double dot(const Vec3& v) const {
 		return x * v.x + y * v.y + z * v.z;
 	}
+
+	Vec3 operator/(const Vec3& v) const {
+		return Vec3(x / v.x, y / v.y, z / v.z);
+	}
+
 };
 
 Vec3 operator*(double scalar, const Vec3& v) {
 	return Vec3(v.x * scalar, v.y * scalar, v.z * scalar);
 }
 
+struct Hit {
+	double s;
+	Vec3 x;
+	Vec3 n;
 
+	Hit(double s, Vec3 x, Vec3 n) : s(s), x(x), n(n) {}
+};
 
-struct Sphere {
-	Vec3 position;
-	Vec3 scale;
+class Shape {
+public:
 	Vec3 diffuse;
 	Vec3 specular;
 	Vec3 ambient;
 	double exponent;
 
+	Shape(const Vec3& diff, const Vec3& spec, const Vec3& amb, double exp)
+		: diffuse(diff), specular(spec), ambient(amb), exponent(exp) {}
+
+	virtual std::optional<Hit> intersect(const Vec3& rayOrigin, const Vec3& rayDirect) const = 0;
+
+	virtual ~Shape() = default;
+	virtual Vec3 normalAt(const Vec3& point) const = 0;
+};
+
+class Sphere : public Shape {
+public:
+	Vec3 position;
+	Vec3 scale;
+	Vec3 normalAt(const Vec3& point) const override {
+		return (point - position).normalize();
+	}
+
 	Sphere(const Vec3& pos, const Vec3& sc, const Vec3& diff, const Vec3& spec, const Vec3& amb, double exp)
-		: position(pos), scale(sc), diffuse(diff), specular(spec), ambient(amb), exponent(exp) {}
+		: Shape(diff, spec, amb, exp), position(pos), scale(sc) {}
+
+	std::optional<Hit> Sphere::intersect(const Vec3& rayOrigin, const Vec3& rayDirect) const override {
+		Vec3 oc = rayOrigin - position;
+		double a = rayDirect.dot(rayDirect);
+		double b = 2.0 * oc.dot(rayDirect);
+		double c = oc.dot(oc) - (scale.x * scale.x);
+		double discriminant = b * b - 4 * a * c;
+		if (discriminant < 0) {
+			return std::nullopt;
+		}
+		double t = (-b - sqrt(discriminant)) / (2 * a);
+		if (t < 0) {
+			t = (-b + sqrt(discriminant)) / (2 * a);
+			if (t < 0) return std::nullopt;
+		}
+		Vec3 hitPoint = rayOrigin + t * rayDirect;
+		Vec3 normal = normalAt(hitPoint);
+		double distance = t;
+		return Hit(distance, hitPoint, normal);
+	}
+
+};
+
+class Ellipsoid : public Shape {
+public:
+	Vec3 position;
+	Vec3 scale;
+	Vec3 normalAt(const Vec3& point) const override {
+		Vec3 normalizedPoint = (point - position) / scale;
+		return Vec3(normalizedPoint.x / scale.x, normalizedPoint.y / scale.y, normalizedPoint.z / scale.z).normalize();
+	}
+
+	Ellipsoid(const Vec3& pos, const Vec3& sc, const Vec3& diff, const Vec3& spec, const Vec3& amb, double exp)
+		: Shape(diff, spec, amb, exp), position(pos), scale(sc) {}
+
+	std::optional<Hit> Ellipsoid::intersect(const Vec3& rayOrigin, const Vec3& rayDirect) const override {
+		Vec3 oc = (rayOrigin - position) / scale;
+		Vec3 rd = rayDirect / scale;
+		double a = rd.dot(rd);
+		double b = 2.0 * oc.dot(rd);
+		double c = oc.dot(oc) - 1.0;
+		double discriminant = b * b - 4 * a * c;
+		if (discriminant < 0) {
+			return std::nullopt;
+		}
+		double t = (-b - sqrt(discriminant)) / (2 * a);
+		if (t < 0) {
+			t = (-b + sqrt(discriminant)) / (2 * a);
+			if (t < 0) return std::nullopt;
+		}
+		Vec3 hitPoint = rayOrigin + t * rayDirect;
+		Vec3 normal = normalAt(hitPoint);
+		double distance = t;
+		return Hit(distance, hitPoint, normal);
+	}
+};
+
+class Plane : public Shape {
+public:
+	Vec3 position;
+	Vec3 normal;
+	Vec3 normalAt(const Vec3&) const override {
+		return normal;
+	}
+
+	Plane(const Vec3& pos, const Vec3& norm, const Vec3& diff, const Vec3& spec, const Vec3& amb, double exp)
+		: Shape(diff, spec, amb, exp), position(pos), normal(norm) {}
+
+	std::optional<Hit> Plane::intersect(const Vec3& rayOrigin, const Vec3& rayDirect) const override {
+		double denom = normal.dot(rayDirect);
+		if (std::abs(denom) > EPSILON) {
+			Vec3 p0l0 = position - rayOrigin;
+			double t = p0l0.dot(normal) / denom;
+			if (t >= 0) {
+				Vec3 hitPoint = rayOrigin + t * rayDirect;
+				Vec3 normalAtPoint = normal;
+				return Hit(t, hitPoint, normalAtPoint);
+			}
+		}
+		return std::nullopt;
+	}
+
+
 };
 
 struct Light {
@@ -80,28 +191,6 @@ struct Light {
 	Light(const Vec3& p, double i) : position(p), intensity(i) {}
 };
 
-
-std::optional<Vec3> ray_and_sphere_intersect(const Vec3& rayOrigin, const Vec3& rayDirect, const Sphere& sphere) {
-	Vec3 oc = rayOrigin - sphere.position;
-	double a = rayDirect.dot(rayDirect);
-	double b = 2.0 * oc.dot(rayDirect);
-	double c = oc.dot(oc) - (sphere.scale.x * sphere.scale.x);
-	double discrim = b * b - 4 * a * c;
-
-	if (discrim < 0) {
-		return {};
-	}
-
-	double dist = (-b - sqrt(discrim)) / (2.0 * a);
-	if (dist < 0) {
-		dist = (-b + sqrt(discrim)) / (2.0 * a);
-	}
-	if (dist < 0) {
-		return {};
-	}
-
-	return rayOrigin + rayDirect * dist;
-}
 
 Vec3 blinnPhong(const Vec3& normal, const Vec3& hitPoint, const Light& light,
 				const Vec3& diffuseColor, const Vec3& specularColor,
@@ -149,19 +238,18 @@ vector<Vec3> generate_rays(int width, int height, Vec3 cameraPos, double fovDegr
 	return rays;
 }
 
+double length(const Vec3& v) {
+	return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
 
 
-const double EPSILON = 1e-5;
 
-bool isShadowed(const Vec3& point, const Vec3& lightDir, const vector<Sphere>& spheres, const double lightDistance) {
-	for (const auto& sphere : spheres) {
-		auto shadowIntersect = ray_and_sphere_intersect(point + lightDir * EPSILON, lightDir, sphere);
+bool isShadowed(const Vec3& point, const Vec3& lightDir, const std::vector<std::unique_ptr<Shape>>& shapes, const double lightDist) {
+	for (const auto& shape : shapes) {
+		auto shadowIntersect = shape->intersect(point + lightDir * EPSILON, lightDir);
 		if (shadowIntersect) {
-			Vec3 shadowPoint = shadowIntersect.value();
-			double shadowDist = sqrt((shadowPoint - point).x * (shadowPoint - point).x +
-				(shadowPoint - point).y * (shadowPoint - point).y +
-				(shadowPoint - point).z * (shadowPoint - point).z);
-			if (shadowDist < lightDistance) {
+			Hit shadowHit = shadowIntersect.value();
+			if (shadowHit.s < lightDist) {
 				return true;
 			}
 		}
@@ -169,10 +257,7 @@ bool isShadowed(const Vec3& point, const Vec3& lightDir, const vector<Sphere>& s
 	return false;
 }
 
-// Function to compute the length of a vector
-double length(const Vec3& v) {
-	return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
-}
+
 
 
 
@@ -211,17 +296,23 @@ int main(int argc, char **argv)
 	double zPlane = 4.0;
 	vector<Vec3> rays = generate_rays(imageSize, imageSize, cameraPos, fov, zPlane);
 
-	std::vector<Sphere> spheres;
+	std::vector<std::unique_ptr<Shape>> shapes;
 	std::vector<Light> lights;
 
 	if (scene == 1 || scene == 2) {
-		spheres.push_back(Sphere(Vec3(-0.5, -1.0, 1.0), Vec3(1.0, 1.0, 1.0), Vec3(1.0, 0.0, 0.0), Vec3(1.0, 1.0, 0.5), Vec3(0.1, 0.1, 0.1), 100.0));
-		spheres.push_back(Sphere(Vec3(0.5, -1.0, -1.0), Vec3(1.0, 1.0, 1.0), Vec3(0.0, 1.0, 0.0), Vec3(1.0, 1.0, 0.5), Vec3(0.1, 0.1, 0.1), 100.0));
-		spheres.push_back(Sphere(Vec3(0.0, 1.0, 0.0), Vec3(1.0, 1.0, 1.0), Vec3(0.0, 0.0, 1.0), Vec3(1.0, 1.0, 0.5), Vec3(0.1, 0.1, 0.1), 100.0));
+		shapes.push_back(std::make_unique<Sphere>(Vec3(-0.5, -1.0, 1.0), Vec3(1.0, 1.0, 1.0), Vec3(1.0, 0.0, 0.0), Vec3(1.0, 1.0, 0.5), Vec3(0.1, 0.1, 0.1), 100.0)); //Red Sphere
+		shapes.push_back(std::make_unique<Sphere>(Vec3(0.5, -1.0, -1.0), Vec3(1.0, 1.0, 1.0), Vec3(0.0, 1.0, 0.0), Vec3(1.0, 1.0, 0.5), Vec3(0.1, 0.1, 0.1), 100.0)); //Green Sphere
+		shapes.push_back(std::make_unique<Sphere>(Vec3(0.0, 1.0, 0.0), Vec3(1.0, 1.0, 1.0), Vec3(0.0, 0.0, 1.0), Vec3(1.0, 1.0, 0.5), Vec3(0.1, 0.1, 0.1), 100.0)); //Blue Sphere
 		lights.push_back(Light(Vec3(-2.0, 1.0, 1.0), 1.0));
 	}
 	else if (scene == 3) {
-		std::cout << "Scene 3 setup is not implemented yet." << std::endl;
+		lights.push_back(Light(Vec3(1.0, 2.0, 2.0), 0.5));
+		lights.push_back(Light(Vec3(-1.0, 2.0, -1.0), 0.5));
+		
+
+		shapes.push_back(std::make_unique<Ellipsoid>(Vec3(0.5, 0.0, 0.5), Vec3(0.5, 0.6, 0.2), Vec3(1.0, 0.0, 0.0), Vec3(1.0, 1.0, 0.5), Vec3(0.1, 0.1, 0.1), 100.0)); //Red Ellipse
+		shapes.push_back(std::make_unique<Sphere>(Vec3(-0.5, 0.0, -0.5), Vec3(1.0, 1.0, 1.0), Vec3(0.0, 1.0, 0.0), Vec3(1.0, 1.0, 0.5), Vec3(0.1, 0.1, 0.1), 100.0)); //Green Sphere
+		shapes.push_back(std::make_unique<Plane>(Vec3(0.0, -1.0, 0.0), Vec3(0.0, 1.0, 0.0), Vec3(1.0, 1.0, 1.0), Vec3(0.0, 0.0, 0.0), Vec3(0.1, 0.1, 0.1), 0.0)); //Flat Plane
 	}
 
 
@@ -231,34 +322,33 @@ int main(int argc, char **argv)
 			Vec3 pixColor(0.0, 0.0, 0.0);
 			double minDist = std::numeric_limits<double>::infinity();
 
-			for (const auto& sphere : spheres) {
-				auto hitPointOpt = ray_and_sphere_intersect(cameraPos, rayDirect, sphere);
+			for (const auto& shape : shapes) {
+				auto hitOpt = shape->intersect(cameraPos, rayDirect);
 
-				if (hitPointOpt) {
-					Vec3 hitPoint = hitPointOpt.value();
-					Vec3 N = (hitPoint - sphere.position).normalize();
-					double distance = length(hitPoint - cameraPos);
-
+				if (hitOpt) {
+					Hit hit = hitOpt.value();
+					double distance = hit.s;
 					if (distance < minDist) {
 						minDist = distance;
-						Vec3 accumulatedColor(0.0, 0.0, 0.0);
+						Vec3 accumColor(0.0, 0.0, 0.0);
 
 						for (const auto& light : lights) {
-							Vec3 toLight = (light.position - hitPoint).normalize();
-							double lightDistance = length(light.position - hitPoint);
+							Vec3 toLight = (light.position - hit.x).normalize();
+							double lightDist = length(light.position - hit.x);
 
-							if (!isShadowed(hitPoint, toLight, spheres, lightDistance) || scene == 1) {
-								accumulatedColor = accumulatedColor + blinnPhong(N, hitPoint, light, sphere.diffuse, sphere.specular, sphere.ambient, sphere.exponent, cameraPos);
+							if (!isShadowed(hit.x, toLight, shapes, lightDist) || scene == 1) {
+								accumColor = accumColor + blinnPhong(hit.n, hit.x, light, shape->diffuse, shape->specular, shape->ambient, shape->exponent, cameraPos);
 							}
 							else {
-								accumulatedColor = accumulatedColor + sphere.ambient * light.intensity;
+								accumColor = accumColor + shape->ambient * light.intensity;
 							}
 						}
 
-						pixColor = accumulatedColor;
+						pixColor = accumColor;
 					}
 				}
 			}
+
 
 			pixColor.x = std::min(std::max(pixColor.x, 0.0), 1.0);
 			pixColor.y = std::min(std::max(pixColor.y, 0.0), 1.0);
